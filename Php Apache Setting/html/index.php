11852216a6ae07e9150993db5d8c946863619f77
Guillermo Albornoz <cachoalbornoz@gmail.com>
2023-03-09 12:40:05 -0300

    Php Apache - Setting

---
+++ Php Apache Setting/html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker - Apache</title>
+</head>
+
+<body>
+    <div style="font-size:40px; text-align:center">
+        Docker + Apache con PHP
+    </div>
+</body>
+
+</html>
--- Php Apache/html/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-print 'Bienvenidos a Docker Compose';
